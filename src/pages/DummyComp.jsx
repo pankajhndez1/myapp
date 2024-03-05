cff88dcb4e90da67561fbb46d0670b5bdaa240bc
pankaj kumar <pankajkumar12861@gmail.com>
2024-03-06 01:07:28 +0530

    added the nested hoc

--- src/pages/DummyComp.jsx
+++ src/pages/DummyComp.jsx
@@ -4,7 +4,8 @@ import MyNewEnhanceComp from './NestedHoc'
 const DummyComp = ({ text, age, data }) => {
     console.log(text, age, data, '<===text, age, data');
     return (
-        <div className='flex flex-col justify-center items-center'>
+        <div className='flex flex-col justify-center items-center space-y-10 mt-5'>
+            <p className='text-2xl font-semibold'>MyNewEnhanceComp Hoc Component used</p>
             <p className='text-2xl font-semibold'>Really important in good projects !!!</p>
             <p className='text-2xl font-semibold'>{text}</p>
             <p className='text-2xl font-semibold'>{data}</p>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import AddBgToAllThePages from './HocComp';
 const Home = ({ permissions,classNames,name }) => {
   return (
     <div className={classNames}>
+      <p className='flex justify-center items-center text-3xl font-medium'>AddBgToAllThePages Hoc Component used</p>
       <h1 className='flex justify-center items-center text-3xl py-6'>Home Component</h1>
       <h2  className='flex justify-center items-center text-2xl py-6'>Name is {name}</h2>
       <h1  className='flex justify-center items-center text-2xl py-6'>{permissions ? "Permissions are coming from Hoc" : "No permissions"}</h1>
