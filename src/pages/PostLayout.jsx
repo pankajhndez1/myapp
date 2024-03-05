b2e863ac943e2ba2d637d0ec23f5ebae6800f6c4
pankaj kumar <pankajkumar12861@gmail.com>
2024-03-05 22:59:03 +0530

    minor change

--- src/pages/PostLayout.jsx
+++ src/pages/PostLayout.jsx
@@ -4,7 +4,8 @@ import { Outlet } from 'react-router-dom'
 const PostLayout = () => {
   return (
     <div>
-        <h1 className='flex justify-center items-center text-3xl py-6'>This is PostLayout</h1>
+        <h1 className='flex justify-center items-center text-3xl py-6'>
+          This is PostLayout</h1>
         <Outlet/>
     </div>
   )
--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 
 const PostPage = () => {
-  const { route='no route' } = useParams();
-  console.log(route,'<===route');
+  const { route = 'no route' } = useParams();
+  console.log(route, '<===route');
   return (
     <div>
       <h1 className='flex justify-center items-center text-3xl py-6'>This is PostPage</h1>
