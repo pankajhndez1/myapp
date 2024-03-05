0ee446cd299159b76a3bfb0dc1a7b8fca87d0e86
pankaj kumar <pankajkumar12861@gmail.com>
2024-03-05 23:43:45 +0530

    adding permissions

--- src/pages/HocComp.jsx
+++ src/pages/HocComp.jsx
@@ -1,9 +1,11 @@
 const AddBgToAllThePages = Comp => {
-    console.log(Comp,'<====Comp');
+    console.log(Comp, '<====Comp');
     return (
         function HocComp() {
             return (
-                <div><Comp permissions={"permissons"} classNames="bg-gray-200"/></div>
+                <div>
+                    <Comp permissions={"permissons"} classNames="bg-gray-200" />
+                </div>
             )
         }
     )
